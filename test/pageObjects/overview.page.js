b681478f78d16446dbcd45d3053d258fedeb0c67
Habib Akbar Wibowo <hbibakber@gmail.com>
2024-09-15 03:06:48 +0700

    Add object for overview.page.js

--- test/pageObjects/overview.page.js
+++ test/pageObjects/overview.page.js
@@ -2,6 +2,21 @@ const { $ } = require('@wdio/globals')
 
 class OverviewPage {
 
+    get overviewPageTitle () {
+        return $('//span[contains(@data-test,"title") and text() = "Checkout: Overview"]')
+    }
+
+    get productName () {
+        return $('//div[contains(@data-test,"inventory-item-name") and text()="Sauce Labs Backpack"]')
+    }
+
+    get btnCancel () {
+        return $('//button[contains(@id,"cancel") and text() = "Cancel"]')
+    }
+
+    get btnFinish () {
+        return $('//button[@id="finish"]')
+    }
 }
 
 module.exports = new OverviewPage()
